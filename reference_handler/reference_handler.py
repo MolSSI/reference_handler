29f81b0dd5fe46e7c8b97004c92503e026c0bc3f
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2019-08-18 15:55:41 -0400

    First commit

--- reference_handler/reference_handler.py
+++ reference_handler/reference_handler.py
@@ -1,34 +1,208 @@
 """
+#toodo make lowercase name of tables
+#read about index
+#primary keys in more fields of the context
+# PRAGMA foreign_keys = ON maybe
+# do not use autoincrement http://www.sqlitetutorial.net/sqlite-autoincrement/
+# cursor.rowcount()
+# make sure some arguments are optional and required 
+#in get reference id maybe change nested ifs for sqlite OR AND next to WHEReE\
+# make input a dictionary to cite and use kwargs and stuff to make a nice API
+# get rid of class reference_handler and make ti just a module
+# clean the total_context and total_citations return values to make an int
+# get rid of doi as searching option
+
 reference_handler.py
 A Python package that facilitates the citation of scientific material.
 
 Handles the primary functions
 """
 
+import sqlite3
+import os.path
+import random
+import string
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
+class Reference_Handler(object):
 
-    Replace this function and doc string for your own project
+    def __init__(self, database):
 
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+        self.conn = sqlite3.connect(database)
+        self.cur = self.conn.cursor()
+        self._initialize_tables()
 
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
+    
+    def cite(self, raw=None, module=None, level=None, note=None, doi=None):
+        """
+        Placeholder function to show example docstring (NumPy format)
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+        Replace this function and doc string for your own project
+
+        Parameters
+        ----------
+        with_attribution : bool, Optional, default: True
+            Set whether or not to display who the quote is from
+
+        Returns
+        -------
+        quote : str
+            Compiled string including quote and optional attribution
+        """
+
+        if raw is None or module is None or level is None or note is None:
+            raise NameError('Need to provide the "raw", "module", "level" and "note" arguments')
+
+        if doi is None:
+            doi = self._extract_doi(raw) 
+
+        reference_id = self._get_reference_id(raw=raw, doi=doi)
+
+        if reference_id is None:
+            self._create_citation(raw=raw, doi=doi)
+            self._create_context(reference_id=self.cur.lastrowid, module=module, note=note, level=level)
+        else:
+            context_id = self._get_context_id(reference_id=reference_id, module=module, note=note, level=level)
+
+            if context_id is None:
+                self._create_context(reference_id=reference_id, module=module, note=note, level=level)
+
+    def _extract_doi(self, raw=None):
+        """ Insert routine to parse DOI from bibliographic format"""
+        string_length = 10
+        letters = string.ascii_lowercase
+        return  ''.join(random.choice(letters) for i in range(string_length))
+        
+    def _initialize_tables(self):
+        
+        self.cur.execute(
+            """CREATE TABLE IF NOT EXISTS "Citation" (
+            "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+            "raw"	TEXT NOT NULL UNIQUE,
+            "doi"	TEXT NOT NULL UNIQUE
+            ); 
+            """
+        )
+        self.cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS "Context" (
+            "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+            "reference_id" INTEGER NOT NULL,
+            "module" TEXT NOT NULL,
+            "note" TEXT NOT NULL,
+            "count"	INTEGER NOT NULL,
+            "level" INTEGER NOT NULL,
+            FOREIGN KEY(reference_id) REFERENCES Citation(id)
+            );
+            """
+        )
+        self.conn.commit()
 
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    def _get_reference_id(self, raw=None, doi=None):
+
+        if raw is None and doi is None:
+            raise NameError('Variables "raw" or "DOI" are required. ')
+
+        if raw is not None:
+            self.cur.execute(
+             """
+             SELECT id FROM Citation WHERE raw=?;
+             """,
+             (raw,)
+            )
+
+        if doi is not None and raw is None:
+            self.cur.execute(
+             """
+             SELECT id FROM Citation WHERE doi=?;
+             """,
+             (doi,)
+            )
+
+        ret = self.cur.fetchall()
+
+        if len(ret) == 0:
+            return None
+
+        return ret[0][0]
+
+    def _get_context_id(self, reference_id=None, module=None, note=None, level=None):
+
+        if reference_id is None or module is None or note is None or level is None:
+            raise NameError('The variables "reference_id" and "module" and "note" and "level" must be specified')
+
+        self.cur.execute(
+            """
+            SELECT id FROM Context WHERE reference_id=? AND module=? AND note=? AND level=?;
+            """, 
+            (reference_id, module, note, level)
+        )
+
+        ret = self.cur.fetchall()
+
+        if len(ret) == 0:
+            return None
+
+        return ret[0][0]
+
+    def _create_citation(self, raw=None, doi=None):
+
+        if raw is None or doi is None:
+            raise NameError('The values for raw and DOI must be provided')
+
+        self.cur.execute(
+            """
+            INSERT INTO Citation (raw, doi) VALUES (?, ?) 
+            """,
+            (raw, doi)
+        )
+
+        self.conn.commit()
+
+    def _create_context(self, reference_id=None, module=None, note=None, level=None):
+
+        if reference_id is None:
+            raise NameError("Variables 'reference_id' or must be specified.")
+
+        self.cur.execute(
+            """
+            INSERT INTO Context (reference_id, module, note, count, level) VALUES (?, ?, ?, ?, ?)
+            """, 
+            (reference_id, module, note, 0, level)
+        )
+
+        self.conn.commit()
+
+    def _print_citations(self):
+        self.cur.execute(
+            """SELECT * FROM Citation"""
+        )
+        print (self.cur.fetchall())
+
+    def __del__(self):
+
+        self.cur.close()
+
+    def total_citations(self):
+
+        self.cur.execute(
+            """SELECT COUNT(*) FROM Citation
+            """)
+
+        return self.cur.fetchall()[0][0]
+
+    def total_contexts(self, reference_id=None):
+
+        if reference_id is None:
+                raise NameError("Variables 'reference_id' must be specified.")
+
+        self.cur.execute(
+            """
+            SELECT COUNT(*) FROM Context WHERE reference_id = ?;
+            """, 
+            (reference_id,) )
+
+        return self.cur.fetchall()[0][0]
+
+    def __str__(self):
+        pass
--- reference_handler/tests/test_reference_handler.py
+++ reference_handler/tests/test_reference_handler.py
@@ -3,10 +3,116 @@ Unit and regression test for the reference_handler package.
 """
 
 # Import package, test suite, and other packages as needed
+import os
 import reference_handler
 import pytest
 import sys
 
+database = '/Users/meliseo/Git/reference_handler/reference_handler/tests/data/database.db'
+
+lammps_citation = """
+        @article{PLIMPTON19951,
+        title = "Fast Parallel Algorithms for Short-Range Molecular Dynamics",
+        journal = "Journal of Computational Physics",
+        volume = "117",
+        number = "1",
+        pages = "1 - 19",
+        year = "1995",
+        issn = "0021-9991",
+        doi = "https://doi.org/10.1006/jcph.1995.1039",
+        url = "http://www.sciencedirect.com/science/article/pii/S002199918571039X",
+        author = "Steve Plimpton",
+        abstract = "Three parallel algorithms for classical molecular dynamics are presented. The first assigns each processor a fixed subset of atoms; the second assigns each a fixed subset of inter-atomic forces to compute; the third assigns each a fixed spatial region. The algorithms are suitable for molecular dynamics models which can be difficult to parallelize efficiently—those with short-range forces where the neighbors of each atom change rapidly. They can be implemented on any distributed-memory parallel machine which allows for message-passing of data between independently executing processors. The algorithms are tested on a standard Lennard-Jones benchmark problem for system sizes ranging from 500 to 100,000,000 atoms on several parallel supercomputers--the nCUBE 2, Intel iPSC/860 and Paragon, and Cray T3D. Comparing the results to the fastest reported vectorized Cray Y-MP and C90 algorithm shows that the current generation of parallel machines is competitive with conventional vector supercomputers even for small problems. For large problems, the spatial algorithm achieves parallel efficiencies of 90% and a 1840-node Intel Paragon performs up to 165 faster than a single Cray C9O processor. Trade-offs between the three algorithms and guidelines for adapting them to more complex molecular dynamics simulations are also discussed."
+    } 
+    """
+
+
+namd_citation = """
+        @article{PHILLIPS2005,
+        author = {Phillips, James C. and Braun, Rosemary and Wang, Wei and Gumbart, James and Tajkhorshid, Emad and Villa, Elizabeth and Chipot, Christophe and Skeel, Robert D. and Kalé, Laxmikant and Schulten, Klaus},
+        title = {Scalable molecular dynamics with NAMD},
+        journal = {Journal of Computational Chemistry},
+        volume = {26},
+        number = {16},
+        pages = {1781-1802},
+        keywords = {biomolecular simulation, molecular dynamics, parallel computing},
+        doi = {10.1002/jcc.20289},
+        url = {https://onlinelibrary.wiley.com/doi/abs/10.1002/jcc.20289},
+        eprint = {https://onlinelibrary.wiley.com/doi/pdf/10.1002/jcc.20289},
+        abstract = {Abstract NAMD is a parallel molecular dynamics code designed for high-performance simulation of large biomolecular systems. NAMD scales to hundreds of processors on high-end parallel platforms, as well as tens of processors on low-cost commodity clusters, and also runs on individual desktop and laptop computers. NAMD works with AMBER and CHARMM potential functions, parameters, and file formats. This article, directed to novices as well as experts, first introduces concepts and methods used in the NAMD program, describing the classical molecular dynamics force field, equations of motion, and integration methods along with the efficient electrostatics evaluation algorithms employed and temperature and pressure controls used. Features for steering the simulation across barriers and for calculating both alchemical and conformational free energy differences are presented. The motivations for and a roadmap to the internal design of NAMD, implemented in C++ and based on Charm++ parallel objects, are outlined. The factors affecting the serial and parallel performance of a simulation are discussed. Finally, typical NAMD use is illustrated with representative applications to a small, a medium, and a large biomolecular system, highlighting particular features of NAMD, for example, the Tcl scripting language. The article also provides a list of the key features of NAMD and discusses the benefits of combining NAMD with the molecular graphics/sequence analysis software VMD and the grid computing/collaboratory software BioCoRE. NAMD is distributed free of charge with source code at www.ks.uiuc.edu. © 2005 Wiley Periodicals, Inc. J Comput Chem 26: 1781–1802, 2005},
+        year = {2005}
+        }
+"""
+
+
+def _create_db():
+    """Boiler plate"""
+    if os.path.exists(database):
+        os.remove(database)
+
+    return reference_handler.Reference_Handler(database)
+    
+    
 def test_reference_handler_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "reference_handler" in sys.modules
+
+def test_initialization():
+
+    rf = _create_db()
+
+    assert rf.total_citations() == 0
+    assert rf.total_contexts(reference_id=1) == 0
+
+def test_add_new_cite_to_empty_db():
+
+    rf = _create_db()
+
+    rf.cite(raw=lammps_citation, module='test_add_new_cite', level=1, note='This is a test')
+
+    assert rf.total_citations() == 1
+    assert rf.total_contexts(reference_id=1) == 1
+
+def test_add_existing_citation():
+
+    rf = _create_db()
+
+    rf.cite(raw=lammps_citation, module='LAMMPS', level=1, note='The main LAMMPS paper')
+    rf.cite(raw=lammps_citation, module='LAMMPS', level=1, note='The main LAMMPS paper')
+
+    assert rf.total_citations() == 1
+    assert rf.total_contexts(reference_id=1) == 1
+
+def test_add_new_context():
+
+    rf = _create_db()
+
+    rf.cite(raw=lammps_citation, module='LAMMPS', level=1, note='Context 1')
+    rf.cite(raw=lammps_citation, module='LAMMPS', level=1, note='Context 2')
+
+    assert rf.total_citations() == 1
+    assert rf.total_contexts(reference_id=1) == 2
+
+    rf.cite(raw=lammps_citation, module='LAMMPS', level=2, note='Context 1')
+
+    assert rf.total_citations() == 1
+    assert rf.total_contexts(reference_id=1) == 3 
+
+    rf.cite(raw=lammps_citation, module='LAMMPS_2', level=2, note='Context 1')
+    assert rf.total_citations() == 1
+    assert rf.total_contexts(reference_id=1) == 4
+
+def test_add_new_cite_to_existing_db():
+
+    rf = _create_db()
+
+    rf.cite(raw=lammps_citation, module='LAMMPS', level=1, note='Context 1')
+    rf.cite(raw=namd_citation, module='NAMD', level=1, note='Context 1')
+
+    assert rf.total_citations() == 2
+    assert rf.total_contexts(reference_id=1) == 1
+    assert rf.total_contexts(reference_id=2) == 1
+
+def test_add_many_cites_and_many_contexts():
+
+    pass
